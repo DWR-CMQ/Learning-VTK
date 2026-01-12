0412fdab1a9d2ab63643101fd22e6e61bb85541a
DWR-CMQ <lr2220549465@163.com>
2026-01-13 04:07:18 +0800

    add imgui-docking

--- MCFrag/main.cpp
+++ MCFrag/main.cpp
@@ -76,9 +76,8 @@ int main(int argc, char* argv[])
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO(); (void)io;
-    //io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
-    //io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows'
-
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
+    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows'
     // Setup Dear ImGui style
     ImGui::StyleColorsDark();
 
